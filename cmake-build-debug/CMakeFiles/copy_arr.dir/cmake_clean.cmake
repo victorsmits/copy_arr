038288ecb574d31d8af1a55658a4419a57db525c
victorsmits <victor.smits@student.vinci.be>
2018-11-21 11:54:13 +0100

    first commit

---
+++ cmake-build-debug/CMakeFiles/copy_arr.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/copy_arr.dir/main.c.o"
+  "copy_arr.pdb"
+  "copy_arr"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/copy_arr.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
